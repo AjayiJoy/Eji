8a2ea511ed426ed6a4a6516f82173d7c39a09589
Blckre <alloyod@gmail.com>
2020-06-27 20:44:56 +0100

    Added StartScreen

---
+++ Assets/Scripts/LoadingScenes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScenes : MonoBehaviour
+{
+    public void StartPlay()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OnQuit()
+    {
+        Debug.Log("Game Quit");
+        //Application.Quit();
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    
+
+    
+}
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -8,45 +8,65 @@ public class Tile : MonoBehaviour
     public float[] xPosition; //Position of the tiles on the tileset
     public float[] yPosition;
     public GameObject no2; //Game object containing the sprite to be generated
+    public GameObject tile; //Original tile
     public List<Vector2> numberOfTimes; //List used to count the amount of times 2 is generated
     public float currentTime;
-    
+   
+
     void Start()
     {
-        // Invoke("Spawner", Random.Range(0, 12));
-        Spawner();
-       // StartCoroutine(Spawn());
+        StartCoroutine(Spawn());
     }
 
     void Update()
     {
-       //Spawner();
-        //while(numberOfTimes.Count == 5)
-        //{
-        //    Spawner();
-        //}
-        //currentTime -= Time.deltaTime ;
-        //if(currentTime <=0)//for (int i = 12; i <= currentTime; i--)
-        //{
-        //    Spawner();
-        //}
-    }
+        currentTime += Time.deltaTime; //Counts the amount of second spent in the game
+        if (currentTime >= 5.0f)
+        {
+            CancelInvoke("Spawner");// stops the spawning
+        }
 
-    //private IEnumerator Spawn()
-    //{
-    ////StopCoroutine(Spawn());
-    //Spawner();
-    //yield return new WaitForSeconds(3);
-    //}
+        if (currentTime >= 5.1f)
+        {
+            CancelInvoke("CoverUp");
+        }
+    }
+   
+
+    private IEnumerator Spawn()
+    {
+        
+        InvokeRepeating("Spawner", 0.04f, 1); //Spawns the number
+        InvokeRepeating("CoverUp", 1.0f, 1); //Covers the number
+        yield return null;
+    }
     void Spawner()
     { 
         int randRow = Random.Range(0, xPosition.Length); //Random generation for the row
         int randColumn = Random.Range(0, yPosition.Length); //Random generation for the column
         Instantiate(no2,  new Vector2(xPosition[randRow], yPosition[randColumn]), Quaternion.identity); //creating clones of no2 tile
         numberOfTimes.Add( new Vector2(xPosition[randRow], yPosition[randColumn]));
-        Debug.Log(numberOfTimes);
+        //Debug.Log(numberOfTimes);
+    }
 
-        //Invoke("Spawner", Random.Range(0, 12));
+    void CoverUp()
+    {
+        //if (numberOfTimes.Count == 1) 
+        //{
+        //    Vector2 place = numberOfTimes[0];
+        //    Instantiate(tile, place, Quaternion.identity);
+        //}
+        //else 
+        //{
+            for (int i = 0; i < numberOfTimes.Count; i++)
+            {
+                Vector2 places = numberOfTimes[i];
+                Instantiate(tile, places, Quaternion.identity);
+            }
+       // }
+
+
+
     }
 
 }
